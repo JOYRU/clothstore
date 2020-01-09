53e2eb6970ee5a2bca94becd983945ebcdc7401f
Tariqul Islam <tariqulislamrc@gmail.com>
2020-01-09 23:54:55 +0600

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('css')
+@parent
+<link href="{{ asset('assets/css/components.min.css') }}" rel="stylesheet" type="text/css">
+@endsection
+@section('content')
+<div class="container mt-3 mb-3">
+	<div class="row">
+		<div class="col-md-12">
+			<p>
+The business is simple cloth store where you can find your prefered product. You can buy your required product from here.
+
+			</p>
+		</div>
+	</div>
+</div>
+@endsection
